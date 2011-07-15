1fc03cd9a53c78e87167c198affa5834e4e87f19
J. David Mendoza <jdmendoza@um.edu.mx>
2011-07-15 10:29:36 -0500

    Primeros pasos de Traductor

---
+++ grails-app/controllers/contabilidad/TraductorController.groovy
@@ -0,0 +1,27 @@
+package contabilidad
+
+import grails.converters.JSON
+import grails.plugins.springsecurity.Secured
+import groovy.sql.*
+
+@Secured(['ROLE_EMP'])
+class TraductorController {
+
+    def index = { }
+
+    def leer = {
+        def sql = Sql.newInstance("jdbc:oracle:thin:@oreb.um.edu.mx:1521:oreb", "noe","arcopacto", "oracle.jdbc.driver.OracleDriver")
+        def movimientos = new StringBuilder()
+        movimientos.append("<table><tbody>")
+        sql.eachRow("select * from mateo.cont_movimiento where fecha > to_date('20-06-2011','dd-mm-yyyy')") { row ->
+            movimientos.append("<tr>")
+            movimientos.append("<td>")
+            movimientos.append(row) 
+            movimientos.append("</td>")
+            movimientos.append("</tr>")
+        }
+        movimientos.append("</tbody></table>")
+        sql.close()
+        render movimientos.toString()
+    }
+}
